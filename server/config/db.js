269ca2696ec2b7f1d724df3bf52522b37bac0810
BU Inc <bawasen12@gmail.com>
2025-02-27 23:29:52 +0530

    Minor changes

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
   // host: process.env.DB_HOST || '127.0.0.1',
-  host: process.env.DB_HOST || 'db',
+  host: process.env.DB_HOST || 'db:3306',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASS || '',
   database: process.env.DB_NAME || 'todo_db',
