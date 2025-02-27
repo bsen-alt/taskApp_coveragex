4f044c3ac7beb4c69459776f5b12e09010843348
BU Inc <bawasen12@gmail.com>
2025-02-27 23:38:20 +0530

    Minor changes to db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,8 +2,7 @@ require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  // host: process.env.DB_HOST || '127.0.0.1',
-  host: process.env.DB_HOST || 'db:3306',
+  host: process.env.DB_HOST || 'db',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASS || '',
   database: process.env.DB_NAME || 'todo_db',
@@ -13,9 +12,26 @@ const pool = mysql.createPool({
   queueLimit: 0
 });
 
-// Test the connection
-pool.getConnection()
-  .then(() => console.log('MySQL Database Connected'))
-  .catch(err => console.error('MySQL Connection Error:', err));
+// Function to test the connection with retries
+async function connectWithRetry() {
+  let retries = 5;
+  while (retries) {
+    try {
+      await pool.getConnection();
+      console.log('MySQL Database Connected');
+      break;
+    } catch (err) {
+      console.error('MySQL Connection Error:', err);
+      retries -= 1;
+      if (retries === 0) {
+        throw new Error('Unable to connect to MySQL after several attempts');
+      }
+      console.log('Retrying in 5 seconds...');
+      await new Promise(res => setTimeout(res, 5000));
+    }
+  }
+}
+
+connectWithRetry();
 
 module.exports = pool;
